1a9fa9f102fdf71aac08a5566de47e56b0a72784
Tony Bengué <tonybengue@hotmail.fr>
2019-11-03 12:28:44 +0100

    Ajout de l'user et du formulaire de connexion

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-
 class DefaultController extends AbstractController {
     public function index(){
         return $this->render('Default/index.html.twig');

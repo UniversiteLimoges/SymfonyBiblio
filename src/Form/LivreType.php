85421f00e7deed7e0fa558c79688945f06d1ea5b
Calas <louiscalas@hotmail.fr>
2019-11-07 15:26:53 +0100

    "modifcation de la class livre, ajout d'un lien de type many to many entre le classe livre et user, creation du crud de livre"

---
+++ src/Form/LivreType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Livre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('auteur')
+            ->add('editeur')
+            ->add('dateParution')
+            ->add('disponibilite')
+            ->add('iban')
+            ->add('exemplaire_total')
+            ->add('exemplaire_dispo')
+            ->add('category')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
